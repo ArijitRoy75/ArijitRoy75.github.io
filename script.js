5fc1b9122ea248d87f38d1f66324099e990d18f9
Arijit Ray <35370469+ArijitRoy75@users.noreply.github.com>
2022-01-27 17:49:16 +0530

    Create script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const linkInfo={
     "github":"https://github.com/ArijitRoy75",
     "linkedin":"https://www.linkedin.com/in/arijit-r-71b750a2/",
     "twitter":"https://twitter.com/the_awfish_guy",
-    "facebook":"",
+    "facebook":"https://www.facebook.com/arijit.roy.9237",
     "instagram":"",
     "rotaract":"",
     "mail":"mailto: arijitray.official@gmail.com"
